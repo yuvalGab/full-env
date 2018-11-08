6615e86a3a3951e643653ea9f7f4f551b4b6048f
Yuval <yuval@korentec.co.il>
2018-11-08 16:06:31 +0200

    add mongodb service

--- server1/index.js
+++ server1/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+const { PORT = 3001 } = process.env
+
+mongoose.connect(`mongodb://db:27017/full_env`)
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+
+db.once('open', () => {
+  console.log('server1 connected to db!')
+})
 
 const app = express()
-const { PORT = 3001 } = process.env
 
 app.get('/', (req, res) => {
   res.send('server1')
